07880858f34ca57e21f276f0c571a9719d44fd20
Ramon Torres <ramon.torres@edgebound.com>
2012-06-25 12:16:54 -0500

    refactoring for better OO design
    Refactoring design for a better object-oriented design
    
    Added explanation comments for the workaround on Pointers of type
    'SCNetworkReachabilityContext'
    
    Implementing callback deregistration on stop_notifier

--- app/reachability.rb
+++ app/reachability.rb
@@ -2,21 +2,7 @@
 
 class Reachability
 
-	@@ids = {}
-
 	attr_reader :callback
-
-	def initialize( target, callback = nil, status_callback = self.method( :network_status ).to_proc )
-		@target = target
-		@callback = callback
-		@determine_status = status_callback
-		ObjectSpace.define_finalizer( self, self.class.method( :finalize ).to_proc )
-	end
-
-	def self.finalize( obj_id )
-		notifier = ObjectSpace._id2ref( obj_id )
-		notifier.stop_notifier if notifier
-	end
 
 	def self.with_hostname( hostname, &callback )
 		ref = SCNetworkReachabilityCreateWithName( KCFAllocatorDefault, hostname.UTF8String )
@@ -37,13 +23,13 @@ class Reachability
 
 	def self.for_wifi( &callback )
 		wifi = Object.new
-		Reachability.with_address( wifi, callback, self.method( :wifi_status ).to_proc )
+		WifiReachability.with_address( wifi, callback )
 	end
 
 	def start_notifier
 		return false if @@ids.key? @target.object_id
-		context_ptr = Pointer.new( SCNetworkReachabilityContext.type )
-		if SCNetworkReachabilitySetCallback( @target, @@callback_handler, context_ptr )
+		@context_ptr = @context_ptr || Pointer.new( SCNetworkReachabilityContext.type )
+		if SCNetworkReachabilitySetCallback( @target, @@callback_handler, @context_ptr )
 			if SCNetworkReachabilityScheduleWithRunLoop( @target, CFRunLoopGetCurrent(), KCFRunLoopDefaultMode )
 				@@ids[ @target.object_id ] = self
 				return true
@@ -54,28 +40,12 @@ class Reachability
 	def stop_notifier
 		return true unless @@ids.key? @target.object_id
 		SCNetworkReachabilityUnscheduleFromRunLoop( @target, CFRunLoopGetCurrent(), KCFRunLoopDefaultMode )
+		SCNetworkReachabilitySetCallback( @target, nil, @context_ptr )
 		@@ids.delete( @target.object_id )
-	end
-
-	@@callback_handler = Proc.new do |target, flags, pointer|
-		return unless @@ids.key? target.object_id
-		notifier = @@ids[ target.object_id ]
-		notifier.instance_variable_set( :@flags, flags )
-		notifier.callback.call( notifier ) unless notifier.callback.nil?
+		@context_ptr = nil
 	end
 
 	def current_status
-		@determine_status.call( @flags )
-	end
-
-	def connection_required?
-		return true unless @flags
-		ftest( KSCNetworkReachabilityFlagsConnectionRequired )
-	end
-
-private
-
-	def network_status
 		return :NotReachable unless ftest( KSCNetworkReachabilityFlagsReachable )
 		return :ReachableViaWWAN if ftest( KSCNetworkReachabilityFlagsIsWWAN )
 		connReq = ftest( KSCNetworkReachabilityFlagsConnectionRequired )
@@ -86,16 +56,64 @@ private
 		:NotReachable
 	end
 
-	def wifi_status
+	def connection_required?
+		return true unless @flags
+		ftest( KSCNetworkReachabilityFlagsConnectionRequired )
+	end
+
+	@@callback_handler = Proc.new do |target, flags, pointer|
+		# workaround as pointer will have a (corrupted)
+		# reference to a Fixnum instance
+		return unless @@ids.key? target.object_id
+		notifier = @@ids[ target.object_id ]
+
+		# NOTE is there way to do this w/o introspection
+		# and without exposing access to instance variable??
+		notifier.instance_variable_set( :@flags, flags )
+
+		# call update block
+		notifier.callback.call( notifier ) unless notifier.callback.nil?
+	end
+
+	def self.finalize( obj_id )
+		notifier = ObjectSpace._id2ref( obj_id )
+		notifier.stop_notifier if notifier && notifier.kind_of?( Reachability )
+	end
+
+protected
+
+	def ftest( reference )
+		return false unless @flags
+		(@flags & reference) != 0
+	end
+
+private
+
+	# workaround to @context_ptr[ 0 ].info
+	# being diferent every time it is accessed
+	@@ids = {}
+
+	def initialize( target, callback = nil )
+		@target = target
+		@callback = callback
+		ObjectSpace.define_finalizer( self, self.class.method( :finalize ).to_proc )
+	end
+
+end
+
+class WifiReachability < Reachability
+
+	def current_status
 		reachable = ftest( KSCNetworkReachabilityFlagsReachable )
 		is_direct = ftest( KSCNetworkReachabilityFlagsIsDirect )
 		return :ReachableViaWiFi if reachable && is_direct
 		:NotReachable
 	end
 
-	def ftest( reference )
-		return false unless @flags
-		(@flags & reference) != 0
+private
+
+	def initialize( target, callback = nil )
+		super( target, callback )
 	end
 
 end
